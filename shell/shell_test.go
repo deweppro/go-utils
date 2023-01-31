219fbb175278e1c9cfac897bb0d06d4521c2e96f
Mikhail Knyazhev <markus621@gmail.com>
2023-01-31 06:16:44 +0300

    contexts

--- shell/shell_test.go
+++ shell/shell_test.go
@@ -14,7 +14,7 @@ func TestUnit_ShellCall(t *testing.T) {
 	sh.SetDir("/tmp")
 	sh.SetEnv("LANG", "en_US.UTF-8")
 
-	out, err := sh.Call(context.TODO(), "ping -c 2 google.ru")
+	out, err := sh.Call(context.TODO(), "ls -la /tmp")
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
@@ -28,7 +28,7 @@ func TestUnit_ShellCallParallelContext(t *testing.T) {
 	sh.SetDir("/tmp")
 	sh.SetEnv("LANG", "en_US.UTF-8")
 	sh.SetWriter(&out)
-	err := sh.CallParallelContext(context.TODO(), "ping -c 2 google.ru", "ping -c 2 yandex.ru", "ls -la")
+	err := sh.CallParallelContext(context.TODO(), "ls -la /tmp", "ls -la /", "ls -la")
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
